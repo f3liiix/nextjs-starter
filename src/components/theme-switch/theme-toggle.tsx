8e1cdb4304affcb1e088eb15e0ccec0da1f3b655
Hoofei <hoofei@gmail.com>
2025-04-23 11:43:51 +0800

    Begin from here

---
+++ src/components/theme-switch/theme-toggle.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import * as React from 'react';
+import { useTheme } from 'next-themes';
+import { Sun } from '@/components/theme-switch/icon-sun';
+import { Moon } from '@/components/theme-switch/icon-moon';
+import { Button } from '@/components/ui/button';
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={toggleTheme}>
+      <Sun className="size-6 scale-100 rotate-0 text-orange-300 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute size-6 scale-0 rotate-90 text-indigo-400 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">切换主题</span>
+    </Button>
+  );
+}
